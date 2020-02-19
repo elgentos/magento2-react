d6342391582cdf83b88cdb94e3a3e72ec772370e
Wouter Steenmeijer <wouter@steenmeijer.nu>
2020-02-19 09:57:58 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,10 @@
-const path = require('path');
-const glob = require('glob');
-const uglify = require('uglifyjs-webpack-plugin');
-
-const appRoot = path.resolve(__dirname, 'app', 'code');
-
-const entries = {};
-
-const codePaths = glob.sync("app/code/*/*/view/*/src/*.define.js", {});
-codePaths.forEach(file => {
-    const relativeFile = path.relative(appRoot, file);
-    entries[path.dirname(path.dirname(relativeFile)) + '/web/js/' + path.basename(file.replace('.define.js', ''))] = "./" + file;
-});
-
-const designPaths = glob.sync("app/design/*/*/*/*/src/*.define.js", {});
-designPaths.forEach(file => {
-    const relativeFile = path.relative(appRoot, file);
-    entries[path.dirname(path.dirname(relativeFile)) + '/web/js/' + path.basename(file.replace('.define.js', ''))] = "./" + file;
-});
-
-const vendorPaths = glob.sync("vendor/*/*/view/*/src/*.define.js", {});
-vendorPaths.forEach(file => {
-    const relativeFile = path.relative(appRoot, file);
-    entries[path.dirname(path.dirname(relativeFile)) + '/web/js/' + path.basename(file.replace('.define.js', ''))] = "./" + file;
-});
-
-if (Object.keys(entries).length === 0) {
-    console.error('ERROR: There are no valid files to compile!');
-}
+const path= require('path');
 
 module.exports = (env, arg) => {
     return {
         devtool: 'inline-source-map',
-        entry: entries,
-        externals: ["magento2-react"],
+        entry: path.resolve(__dirname, 'index.js'),
+        externals: ['mage/translate'],
         module: {
             rules: [
                 {
@@ -41,29 +13,17 @@ module.exports = (env, arg) => {
                         loader: "babel-loader",
                         options: {
                             presets: [
-                                '@babel/react',
                                 '@babel/env'
                             ]
                         }
                     }
-                },
+                }
             ]
         },
         output: {
-            path: appRoot,
+            path: path.resolve(__dirname, 'view/base/web/'),
             libraryTarget: "amd",
-            filename: '[name]' + (arg.mode === 'production' ? '.min' : '') + '.js'
-        },
-        optimization: {
-            minimizer: [
-                new uglify({
-                    uglifyOptions: {
-                        mangle: {
-                            reserved: ['__']
-                        }
-                    }
-                })
-            ]
+            filename: 'js/react.bundle' + (arg.mode === 'production' ? '.min' : '') + '.js'
         }
     };
 };
